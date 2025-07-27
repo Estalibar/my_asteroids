f8d6db182ec622556c5434a076f6012719d29660
Estalibar <reinsagersr@yahoo.com>
2025-07-27 13:33:33 -1000

    Created Game Window

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import pygame
-	
+from constants import *
+
 def main():
-    print("Starting Asteroids!")
-    print("Screen width: 1280")
-    print("Screen height: 720")
+	pygame.init
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	while pygame:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+		screen.fill("black")
+		pygame.display.flip()
+	print("Starting Asteroids!")
+	print("Screen width: 1280")
+	print("Screen height: 720")
 
 if __name__ == "__main__":
     main()
